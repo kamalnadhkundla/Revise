399dd63f5486fd93abdc37d71fd040325d249d86
KAMALNADH KUNDLA <kamalnadhkundla@gmail.com>
2024-12-19 21:46:20 -0600

    Auto commit: changes detected in the Java files

--- 5.SpiralMatrix(54).java
+++ 5.SpiralMatrix(54).java
@@ -10,7 +10,6 @@ Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 Output: [1,2,3,6,9,8,7,4,5]
 
  */
-// right->bottom->left-> right // it follows this pattern until we cover all the elements in the matrix.
 import java.util.*;
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
