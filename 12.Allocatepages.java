fa005e66c51b22bbf900c824c2d2c15fd967ebb4
KAMALNADH KUNDLA <kamalnadhkundla@gmail.com>
2025-03-04 19:25:39 -0500

    Allocate pages  minmax

--- 12.Allocatepages.java
+++ 12.Allocatepages.java
@@ -56,3 +56,74 @@ public class 12.Allocatepages {
     }
     
 }
+
+/*Similar questiom
+Given an integer array nums and an integer k, split nums into k non-empty subarrays such that the largest sum of any subarray is minimized.
+
+Return the minimized largest sum of the split.
+
+A subarray is a contiguous part of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [7,2,5,10,8], k = 2
+Output: 18
+Explanation: There are four ways to split nums into two subarrays.
+The best way is to split it into [7,2,5] and [10,8], where the largest sum among the two subarrays is only 18.
+Example 2:
+
+Input: nums = [1,2,3,4,5], k = 2
+Output: 9
+Explanation: There are four ways to split nums into two subarrays.
+The best way is to split it into [1,2,3] and [4,5], where the largest sum among the two subarrays is only 9.
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+0 <= nums[i] <= 106
+1 <= k <= min(50, nums.length)
+ * 
+ * 
+ */
+class Solution {
+    public int splitArray(int[] arr, int k) {
+      int ans=0;
+
+        int low=Arrays.stream(arr).max().getAsInt();
+        int high=Arrays.stream(arr).sum();
+        while(low<=high){
+            int mid=(low+high)/2;
+
+            if(possible(arr,k,mid))
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+               low=mid+1;
+        }
+
+        return ans;
+        
+    }
+    public boolean possible(int[] arr,int k, int max){
+
+        int count=1;
+        int sum=0;
+        for(int i =0;i<arr.length;i++){
+            if(sum+arr[i]>max)
+            {
+                sum=arr[i];
+                count++;
+                if(count>k) return false;
+            }
+            else
+               sum+=arr[i];
+
+        }
+        return true;
+    }
+}
