bf2a351d8aabeb8653d9ec2823ecc5e26dfc0b33
KAMALNADH KUNDLA <kamalnadhkundla@gmail.com>
2025-03-18 12:41:29 -0500

    Auto commit: changes detected in the Java files

---
+++ LinkedList/2.startingpoint.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+public class 2.startingpoint {
+   
+        public ListNode detectCycle(ListNode head) {
+            ListNode slow = head, fast = head;
+    
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+    
+                if (slow == fast) break;
+            }
+    
+            if (fast == null || fast.next == null) return null;
+    
+            fast=head;
+    
+            while(fast!=slow){
+                fast=fast.next;
+                slow=slow.next;
+            }
+    
+          
+    
+            return slow;        
+        }
+    }
+    
+
