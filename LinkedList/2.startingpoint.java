a1f5c06cb2fad274db3f2aa8ff691971bf1e596d
KAMALNADH KUNDLA <kamalnadhkundla@gmail.com>
2025-03-18 12:41:52 -0500

    Auto commit: changes detected in the Java files

--- LinkedList/2.startingpoint.java
+++ LinkedList/2.startingpoint.java
@@ -26,5 +26,32 @@ public class 2.startingpoint {
             return slow;        
         }
     }
-    
+/*
+ * 
+ * class Solution {
+    // Function to find the length of a loop in the linked list.
+    public int countNodesinLoop(Node head) {
+        // Add your code here.
+        
+        Node fast=head;
+        Node slow= head;
+        
+        while(fast!=null && fast.next!=null) {
+           fast=fast.next.next;
+           slow= slow.next;
+           if(fast==slow) break;
+           
+        }
+        if(fast==null || fast.next==null ) return 0;
+        slow=slow.next;
+        int count=1;
+        while(slow!=fast){
+            slow=slow.next;
+            count++;
+        }
+        return count;
+        
+    }
+}
+ */
 
