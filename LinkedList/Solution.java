94a5fdf3e5d2c1b56b317b61b7e1b45beff15712
KAMALNADH KUNDLA <kamalnadhkundla@gmail.com>
2025-03-18 12:37:14 -0500

    Auto commit: changes detected in the Java files

---
+++ LinkedList/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null) return false;
+        ListNode Fast=head;
+        ListNode Slow = head;
+        
+        while(Fast.next!=null && Fast.next.next!=null)
+        {
+            Fast = Fast.next.next;
+            Slow = Slow.next;
+            if(Fast==Slow) return true;
+        }
+        return false;
+    }
+}
+
+// this is tortoise method
