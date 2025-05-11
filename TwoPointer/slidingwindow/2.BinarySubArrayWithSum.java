23e2bc82e12399f4da4176c7bc02b95de13bfa08
KAMALNADH KUNDLA <kamalnadhkundla@gmail.com>
2025-05-11 12:40:35 -0400

    updated sliding Window

--- TwoPointer/slidingwindow/2.BinarySubArrayWithSum.java
+++ TwoPointer/slidingwindow/2.BinarySubArrayWithSum.java
@@ -73,3 +73,68 @@ public class 2.BinarySubArrayWithSum {
                 return count;
     
 }
+
+/*
+ * count of nice subarrays works in the sameway
+ * Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k odd numbers on it.
+
+Return the number of nice sub-arrays.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2,1,1], k = 3
+Output: 2
+Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
+Example 2:
+
+Input: nums = [2,4,6], k = 1
+Output: 0
+Explanation: There are no odd numbers in the array.
+Example 3:
+
+Input: nums = [2,2,2,1,2,2,1,2,2,2], k = 2
+Output: 16
+
+sliding window solution: 
+public int numberOfSubarrays(int[] nums, int k) {
+        
+    return calculatesubarrays(nums,k)-calculatesubarrays(nums,k-1);
+        
+    }
+    public int calculatesubarrays(int[] nums, int sum){
+        if(sum<0) return 0;
+
+        int left=0;
+        int csum=0;
+        int count=0;
+        for(int right =0;right<nums.length;right++){
+            if(nums[right]%2!=0)
+               csum+=1;
+            while(csum>sum && left<nums.length) {
+                if(nums[left]%2!=0)
+                   csum-=1;
+                left++;
+            }
+            count+=right-left+1;
+        }
+        return count;
+    }
+
+    a small change to hashbased prefix sum why dont you convert it into  binary subarray if even 0 if odd 1 then using prefix sum that equal to k odd will give the answer
+     int numberOfSubarrays(vector<int>& nums, int k) {
+        for(int i = 0; i < nums.size(); i++) {
+            nums[i] %= 2;
+        }
+        
+        vector<int> prefixCounnt(nums.size() + 1);
+        prefixCounnt[0] = 1;
+        int s = 0, ans = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            s += nums[i];
+            ans += (s >= k) ? prefixCounnt[s - k] : 0;
+            prefixCounnt[s]++;
+        }
+        return ans;
+ */
